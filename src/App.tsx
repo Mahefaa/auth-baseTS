b99cfddb95567f5e9d8540cf1bfa79b9f527a0de
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-08-16 17:16:45 +0300

    feat project restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login";
+import Login from "./Pages/Login";
 import {initializeApp} from "firebase/app";
-import {config} from "./components/Login/config";
-initializeApp(config.firebaseConfig);
+import {config} from "./Pages/Login/config";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import AuthRoute from "./Pages/AuthRoute";
+const app = initializeApp(config.firebaseConfig);
 function App() {
   return (
     <div className="App">
-      <Login/>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={
+                    <AuthRoute>
+                        <HomePage app={app}/>
+                    </AuthRoute>
+                }/>
+                <Route path={"/login"} element={
+                    <Login/>
+                }/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+const Loading:React.FC<{}> = (props) =>{
+    return(
+        <div>
+        </div>
+    )
+}
+export default Loading;
